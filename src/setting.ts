b14e9e0ae1a1c6ba298c55b5032b92a3f90e006c
qianchao <jingqianchao163@163.com>
2024-02-26 23:35:40 +0800

    set response strut

--- src/setting.ts
+++ src/setting.ts
@@ -8,190 +8,190 @@ import {logo_image} from "./assets/engine-logo/logos";
 import {TranslateResponse} from "./translate/const/translate-response";
 
 export interface DictionarySettings {
-	engine: keyof typeof TranslateEngines;
-	lang: LangTypeAndAuto,
-	engineConfig: EngineConfig
+    engine: keyof typeof TranslateEngines;
+    lang: LangTypeAndAuto,
+    engineConfig: EngineConfig
 }
 
 export const DEFAULT_SETTINGS: DictionarySettings = {
-	engine: "youdao",
-	lang: "auto",
-	engineConfig: new YoudaoConfigs("", "")
+    engine: "youdao",
+    lang: "auto",
+    engineConfig: new YoudaoConfigs("", "")
 }
 
 export class DictionarySettingTab extends PluginSettingTab {
-	plugin: DictionaryPlugin;
+    plugin: DictionaryPlugin;
 
-	constructor(app: App, plugin: DictionaryPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: DictionaryPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
+    display(): void {
 
-		const {containerEl} = this;
+        const {containerEl} = this;
 
-		containerEl.empty();
-		const i18n = (x: I18nKey, vars?: any) => {
-			return this.plugin.i18n.t(x, vars);
-		};
+        containerEl.empty();
+        const i18n = (x: I18nKey, vars?: any) => {
+            return this.plugin.i18n.t(x, vars);
+        };
 
-		// title
-		containerEl.createEl("h1", {text: "Dictionary Settings"});
+        // title
+        containerEl.createEl("h1", {text: "Dictionary Settings"});
 
-		// Div : enginesChooserDiv
-		const enginesChooserDiv = containerEl.createDiv();
+        // Div : enginesChooserDiv
+        const enginesChooserDiv = containerEl.createDiv();
 
-		/**
-		 * ==========
-		 * ==youdao==
-		 * ==========
-		 */
-		const youdaoEngineDiv = containerEl.createDiv();
-		youdaoEngineDiv.toggleClass("settings-hide", this.plugin.settings.engine !== "youdao")
-		const youdaoHeader = youdaoEngineDiv.createDiv({cls: "setting-engine-header"});
-		youdaoHeader.createEl("span", {text: i18n("guide", {engine: i18n("youdao")}) + ":", cls: "guide"})
-		youdaoHeader.createEl("img", {attr: {src: logo_image.youdao}})
-		const youdaoGuideP = youdaoEngineDiv.createEl("p");
-		youdaoGuideP.createEl("a", {
-			href: "https://ai.youdao.com/console/",
-			text: i18n("youdao_console"),
-		});
-		youdaoGuideP.createEl("span", {text: i18n("youdao_guide")})
+        /**
+         * ==========
+         * ==youdao==
+         * ==========
+         */
+        const youdaoEngineDiv = containerEl.createDiv();
+        youdaoEngineDiv.toggleClass("settings-hide", this.plugin.settings.engine !== "youdao")
+        const youdaoHeader = youdaoEngineDiv.createDiv({cls: "setting-engine-header"});
+        youdaoHeader.createEl("span", {text: i18n("guide", {engine: i18n("youdao")}) + ":", cls: "guide"})
+        youdaoHeader.createEl("img", {attr: {src: logo_image.youdao}})
+        const youdaoGuideP = youdaoEngineDiv.createEl("p");
+        youdaoGuideP.createEl("a", {
+            href: "https://ai.youdao.com/console/",
+            text: i18n("youdao_console"),
+        });
+        youdaoGuideP.createEl("span", {text: i18n("youdao_guide")})
 
 
-		new Setting(youdaoEngineDiv)
-			.setName(i18n("youdao_app_key"))
-			.setDesc("AppKey")
-			.addText((text) => {
-				wrapTextWithPasswordHide(text);
-				text
-					.setValue(`${(this.plugin.settings.engineConfig as YoudaoConfigs).appKey ?? ""}`)
-					.onChange(async (value) => {
-						this.setConfigValue(this.plugin.settings.engineConfig, 'appKey', value)
-						await this.plugin.saveSettings();
-					});
-			});
+        new Setting(youdaoEngineDiv)
+            .setName(i18n("youdao_app_key"))
+            .setDesc("AppKey")
+            .addText((text) => {
+                wrapTextWithPasswordHide(text);
+                text
+                    .setValue(`${(this.plugin.settings.engineConfig as YoudaoConfigs).appKey ?? ""}`)
+                    .onChange(async (value) => {
+                        this.setConfigValue(this.plugin.settings.engineConfig, 'appKey', value)
+                        await this.plugin.saveSettings();
+                    });
+            });
 
-		new Setting(youdaoEngineDiv)
-			.setName(i18n("youdao_app_secret"))
-			.setDesc("AppSecret")
-			.addText((text) => {
-				wrapTextWithPasswordHide(text);
-				text
-					.setValue(`${(this.plugin.settings.engineConfig as YoudaoConfigs).appSecret ?? ""}`)
-					.onChange(async (value) => {
-						this.setConfigValue(this.plugin.settings.engineConfig, 'appSecret', value)
-						await this.plugin.saveSettings();
-					});
-			});
+        new Setting(youdaoEngineDiv)
+            .setName(i18n("youdao_app_secret"))
+            .setDesc("AppSecret")
+            .addText((text) => {
+                wrapTextWithPasswordHide(text);
+                text
+                    .setValue(`${(this.plugin.settings.engineConfig as YoudaoConfigs).appSecret ?? ""}`)
+                    .onChange(async (value) => {
+                        this.setConfigValue(this.plugin.settings.engineConfig, 'appSecret', value)
+                        await this.plugin.saveSettings();
+                    });
+            });
 
-		new Setting(containerEl)
-			.setName(i18n("connect_test"))
-			.setDesc(i18n("connect_test_desc"))
-			.addButton((bc) => {
-				bc.setButtonText(i18n("test"));
-				bc.onClick(async evt => {
-					const words = "hello";
-					const translateResponse = await this.plugin.getTranslator()?.translate({
-						from: "en",
-						to: "cn",
-						words
-					});
-					if (translateResponse) {
-						new Notice(`${words} => ${translateResponse}`)
-						console.log(translateResponse)
-					} else {
+        new Setting(containerEl)
+            .setName(i18n("connect_test"))
+            .setDesc(i18n("connect_test_desc"))
+            .addButton((bc) => {
+                bc.setButtonText(i18n("test"));
+                bc.onClick(async evt => {
+                    const words = "hello world";
+                    const translateResponse = await this.plugin.getTranslator()?.translate({
+                        from: "en",
+                        to: "cn",
+                        words
+                    });
+                    console.debug(translateResponse)
+                    if (translateResponse) {
+                        new Notice(`${words}  ➡️   ${translateResponse.translation}`)
+                    } else {
+                        new Notice(i18n("init_engine_exception", {error: "unknown"}))
+                    }
+                })
+            })
 
-					}
-				})
-			})
+        /**
+         * TODO Google Engine
+         * ==========
+         * ==google==
+         * ==========
+         */
+            // const googleEngineDiv = containerEl.createDiv();
+            // googleEngineDiv.toggleClass("settings-hide", this.plugin.settings.engine !== "google")
+            //
+            // googleEngineDiv.createEl("h5", {text: i18n("guide", {engine: i18n("google")}) + ":"})
+            // let googleGuideP = googleEngineDiv.createEl("p");
+            // googleEngineDiv.createEl("a", {
+            //     href: "https://ai.youdao.com/console/",
+            //     text: i18n("youdao_console"),
+            // });
+            // googleEngineDiv.createEl("span", {text: i18n("youdao_guide")})
+            //
+            // new Setting(googleEngineDiv)
+            //     .setName(i18n("youdao_app_key"))
+            //     .setDesc("AppKey")
+            //     .addText((text) => {
+            //         wrapTextWithPasswordHide(text);
+            //         text
+            //             .setValue(`${this.plugin.settings.config['appKey']}`)
+            //             .onChange(async (value) => {
+            //                 this.plugin.settings.config['appKey'] = value
+            //                 await this.plugin.saveSettings();
+            //             });
+            //     });
+            //
 
-		/**
-		 * TODO Google Engine
-		 * ==========
-		 * ==google==
-		 * ==========
-		 */
-			// const googleEngineDiv = containerEl.createDiv();
-			// googleEngineDiv.toggleClass("settings-hide", this.plugin.settings.engine !== "google")
-			//
-			// googleEngineDiv.createEl("h5", {text: i18n("guide", {engine: i18n("google")}) + ":"})
-			// let googleGuideP = googleEngineDiv.createEl("p");
-			// googleEngineDiv.createEl("a", {
-			//     href: "https://ai.youdao.com/console/",
-			//     text: i18n("youdao_console"),
-			// });
-			// googleEngineDiv.createEl("span", {text: i18n("youdao_guide")})
-			//
-			// new Setting(googleEngineDiv)
-			//     .setName(i18n("youdao_app_key"))
-			//     .setDesc("AppKey")
-			//     .addText((text) => {
-			//         wrapTextWithPasswordHide(text);
-			//         text
-			//             .setValue(`${this.plugin.settings.config['appKey']}`)
-			//             .onChange(async (value) => {
-			//                 this.plugin.settings.config['appKey'] = value
-			//                 await this.plugin.saveSettings();
-			//             });
-			//     });
-			//
+        const engine = new Setting(enginesChooserDiv);
+        engine.setName(i18n("translate_engine"))
+            .setDesc(i18n("engines_chooser_div_title"))
+            .addDropdown(cb => cb.addOptions(this.getEnginesOptions())
+                .setValue(`${this.plugin.settings.engine}`)
+                .onChange(async (value) => {
+                    console.log("engine:" + value)
+                    this.plugin.settings.engine = value as SupportEngine;
+                    youdaoEngineDiv.toggleClass("settings-hide", value !== "youdao")
+                    // googleEngineDiv.toggleClass("settings-hide", value !== "google")
+                    await this.plugin.saveSettings();
+                })
+            );
 
-		const engine = new Setting(enginesChooserDiv);
-		engine.setName(i18n("translate_engine"))
-			.setDesc(i18n("engines_chooser_div_title"))
-			.addDropdown(cb => cb.addOptions(this.getEnginesOptions())
-				.setValue(`${this.plugin.settings.engine}`)
-				.onChange(async (value) => {
-					console.log("engine:" + value)
-					this.plugin.settings.engine = value as SupportEngine;
-					youdaoEngineDiv.toggleClass("settings-hide", value !== "youdao")
-					// googleEngineDiv.toggleClass("settings-hide", value !== "google")
-					await this.plugin.saveSettings();
-				})
-			);
+    }
 
-	}
+    private setConfigValue<T, K extends keyof T, V extends T[K]>(obj: T | undefined, key: K, value: V): T {
+        const updatedObj = obj ?? {} as T;
+        updatedObj[key] = value;
+        return updatedObj;
+    }
 
-	private setConfigValue<T, K extends keyof T, V extends T[K]>(obj: T | undefined, key: K, value: V): T {
-		const updatedObj = obj ?? {} as T;
-		updatedObj[key] = value;
-		return updatedObj;
-	}
-
-	private getEnginesOptions(): Record<string, string> {
-		const options: Record<string, string> = {}
-		Object.entries(TranslateEngines).forEach(([key, value]) => {
-			options[key] = this.plugin.i18n.t(key as I18nKey)
-		});
-		return options
-	}
+    private getEnginesOptions(): Record<string, string> {
+        const options: Record<string, string> = {}
+        Object.entries(TranslateEngines).forEach(([key, value]) => {
+            options[key] = this.plugin.i18n.t(key as I18nKey)
+        });
+        return options
+    }
 
 }
 
 const getEyesElements = () => {
-	const eyeEl = createElement(Eye);
-	const eyeOffEl = createElement(EyeOff);
-	return {
-		eye: eyeEl.outerHTML,
-		eyeOff: eyeOffEl.outerHTML,
-	};
+    const eyeEl = createElement(Eye);
+    const eyeOffEl = createElement(EyeOff);
+    return {
+        eye: eyeEl.outerHTML,
+        eyeOff: eyeOffEl.outerHTML,
+    };
 };
 
 const wrapTextWithPasswordHide = (text: TextComponent) => {
-	const {eye, eyeOff} = getEyesElements();
-	const hider = text.inputEl.insertAdjacentElement("afterend", createSpan())!;
-	// the init type of hider is "hidden" === eyeOff === password
-	hider.innerHTML = eyeOff;
-	hider.addEventListener("click", (e) => {
-		const isText = text.inputEl.getAttribute("type") === "text";
-		hider.innerHTML = isText ? eyeOff : eye;
-		text.inputEl.setAttribute("type", isText ? "password" : "text");
-		text.inputEl.focus();
-	});
+    const {eye, eyeOff} = getEyesElements();
+    const hider = text.inputEl.insertAdjacentElement("afterend", createSpan())!;
+    // the init type of hider is "hidden" === eyeOff === password
+    hider.innerHTML = eyeOff;
+    hider.addEventListener("click", (e) => {
+        const isText = text.inputEl.getAttribute("type") === "text";
+        hider.innerHTML = isText ? eyeOff : eye;
+        text.inputEl.setAttribute("type", isText ? "password" : "text");
+        text.inputEl.focus();
+    });
 
-	// the init type of text el is password
-	text.inputEl.setAttribute("type", "password");
-	return text;
+    // the init type of text el is password
+    text.inputEl.setAttribute("type", "password");
+    return text;
 };
