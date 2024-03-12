8b9ed503f1c2fcdd00a350b1ac471f94448e8917
qianchao <jingqianchao163@163.com>
2024-03-12 21:25:55 +0800

    mount modal styles.css

--- src/translate/modal/TranslationModalComponent.vue
+++ src/translate/modal/TranslationModalComponent.vue
@@ -1,15 +1,24 @@
 <template>
-	<n-card :bordered="false" :on-close="closeCallback" closable>
+	<n-card hoverable :bordered="false" :on-close="closeCallback" closable>
 		<template #header>
 			<n-flex class="translate-card-header">
 				<span><a :href="response.link?.[0]">{{ response.source }}</a></span>
-				<span class="icon"> -> </span>
+				<span class="arrows"> → </span>
 				<span>{{ response.translation?.[0] }}</span>
 			</n-flex>
 		</template>
 		<n-flex vertical>
-			<n-flex>
-				<n-flex>{{ response.speeches }}</n-flex>
+			<n-flex justify="start" size="large" :wrap="false">
+				<n-flex class="speech" v-for="speech in response.speeches">
+					<n-tag >
+						{{speech.area}}. /{{speech.phonetic}}/
+						<template #avatar>
+							<n-icon size="20">
+								<svg t="1710241062525" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6056" width="200" height="200"><path d="M512 1024a512 512 0 1 1 512-512 512 512 0 0 1-512 512z m0-992a480 480 0 1 0 480 480A480 480 0 0 0 512 32z m145.744 506.64l-192 127.84A32 32 0 0 1 416 639.888v-255.68a32 32 0 0 1 49.744-26.592l192 127.84a32 32 0 0 1 0 53.184zM620.8 493.248l-150.224-99.088A45.488 45.488 0 0 0 448 389.568v243.376a51.2 51.2 0 0 0 22.576-5.616l150.224-92.576a26.128 26.128 0 0 0 11.712-21.76 22.784 22.784 0 0 0-11.712-19.744z" fill="#8a8a8a" p-id="6057"></path></svg>
+							</n-icon>
+						</template>
+					</n-tag>
+				</n-flex>
 			</n-flex>
 		</n-flex>
 	</n-card>
@@ -20,11 +29,11 @@ import {defineComponent} from 'vue';
 import {TranslateResponse} from '../const/translate-response';
 import DictionaryPlugin from "../../main";
 import {PropType} from "@vue/runtime-core";
-import {NFlex, NIcon, NCard} from "naive-ui"
+import {NFlex, NIcon, NCard, NTag} from "naive-ui"
 
 export default defineComponent({
 	name: 'TranslationModalComponent',
-	components: {NFlex, NIcon, NCard},
+	components: {NFlex, NIcon, NCard,NTag},
 	props: {
 		response: {
 			type: Object as PropType<TranslateResponse>,
